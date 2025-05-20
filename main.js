7b76eacf638be786d30123f999219ce394eb879a
bgilliam04 <bgilliam@knox.edu>
2025-05-20 18:49:18 -0500

    changed up?

--- main.js
+++ main.js
@@ -32,8 +32,8 @@ var modelViewMatrixLoc, projectionMatrixLoc;
 var modelViewMatrix, projectionMatrix;
 
 var eye;
-var at = vec3(0.0, 5.0, 0.0);
-const up = vec3(0.0, 1.0, 0.0);
+var at = vec3(0.0, 1.0, 0.0);
+const up = vec3(0.0, 5.0, 0.0);
 
 
 
